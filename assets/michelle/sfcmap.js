05e674ba54f0357abab10ea3cf7a90f07b85fb2f
Zhihui Analytics <zhihui.analytics@gmail.com>
2023-04-30 14:18:03 -0700

    map

--- assets/michelle/sfcmap.js
+++ assets/michelle/sfcmap.js
@@ -26,7 +26,7 @@ xmlhttp.send();
 
 // render map
 xhr = new XMLHttpRequest();
-xhr.open("GET","/assets/michelle/media/sfcmap.min.svg",false);
+xhr.open("GET","/assets/michelle/sfcmap.svg",false);
 // Following line is just to be on the safe side;
 // not needed if your server delivers SVG with correct MIME type
 xhr.overrideMimeType("image/svg+xml");
@@ -40,28 +40,51 @@ xhr.send("");
 const outlineList = document.querySelectorAll('g[id$="Outline"]');
 for (const i of outlineList.values()) { i.style='display:none'; }
 
-var sites= { 'CCSF': '#f7b95e'
-,'Woh_Hei_Yuen': '#95d1a3'
-, 'Ping_Yuen': '#f07b3b'
-, 'Willie_Wong_Playground': '#95d1a3'
-, 'Spofford_Alley': '#f07b3b'
-, 'Kubla_Khan_Nightclub': '#cf2844'
-, 'Mei_Lun_Yuen': '#f07b3b'
-, 'I-Hotel': '#f07b3b'
-, 'Portsmouth_Square': '##95d1a3'
-, 'Chinese_for_Affirmative_Action': '#f7b95e'
-, 'Chinese_Hospital': '#94cdd3'
+var sites= { 
+  '_41Ross': ['' ,'caption']
+, 'Betty_Ong_Recreation_Center': ['', 'caption'] //Ann?
+, 'Cameron_House': ['', 'caption']
+, 'CCDC': ['', 'caption']
+, 'CCSF': ['#f7b95e', 'caption']
+, 'Chinese_Culture_Center': ['' , 'caption']//cultural? //
+, 'Chinese_for_Affirmative_Action': ['#f7b95e', 'caption']
+, 'Chinese_Hospital': ['#94cdd3', 'caption']
+, 'Chinese_Historical_Society_of_America': ['' , 'caption']
+, 'Chinese_Newcomer_s_Service_Center': ['' , 'caption']
+, 'Chinese_Progressive_Association': ['', 'caption']
+, 'Gum_Moon_Residence_Hall': ['', 'caption']
+, 'Gordon_J_Lau_Elementary_School': ['' , 'caption']//
+, 'I-Hotel': ['#f07b3b', 'caption']
+, 'Kubla_Khan_Nightclub': ['#cf2844', 'caption']
+, 'Mei_Lun_Yuen': ['#f07b3b', 'caption']
+, 'North_Ping_Yuen': ['', 'caption']
+, 'Ping_Yuen': ['#f07b3b', 'caption']
+, 'Portsmouth_Square': ['##95d1a3' , 'caption']//
+, 'Self-Help_for_the_Elderly_-_Geen_Mun_Senior_Center': ['', 'caption'] // Senior Center
+, 'Spofford_Alley': ['#f07b3b', 'caption']
+, 'St_Mary_s_Square': ['' , 'caption']
+, 'West_Ping_Yuen': ['' , 'caption']
+, 'Willie_Wong_Playground': ['#95d1a3', 'caption']
+, 'Woh_Hei_Yuen': ['#95d1a3', 'caption']
+, 'YMCA_Chinatown': ['', 'caption']
 }
 
 $.each(sites,
-function( key, color) {
-  $( "#"+key+"_Site" ).parent().hover( function() { $( "#"+key+"_Outline" ).css("display", "inline"); }, function() { $( "#"+key+"_Outline" ).css("display", "none"); });
+function( key, vals) {
+  if(vals[0] != '') {
+    $( "#"+key+"_Site" ).parent().hover( function() { $( "#"+key+"_Outline" ).css("display", "inline"); }, function() { $( "#"+key+"_Outline" ).css("display", "none"); });
 
-  $("head").append('<style type="text/css"></style>');
-  var hbg = $("head").children(':last');
-  hbg.html('.hbg'+key+'{--bs-popover-header-bg:'+color+';}');
+    $("head").append('<style type="text/css"></style>');
+    var hbg = $("head").children(':last');
+    hbg.html('.hbg'+key+'{--bs-popover-header-color:#ffffff;--bs-popover-header-bg:'+vals[0]+';}');
+  } else {
+    $("head").append('<style type="text/css"></style>');
+    var hbg = $("head").children(':last');
+    hbg.html('.hbg'+key+'{--bs-popover-header-bg:#ffffff;}');
+    $("#"+key+"_Site").parent().css('cursor','default')
+  }
 
-  new bootstrap.Popover($("#"+key+"_Site").parent(), { trigger:'hover', placement:'top', title : $("#"+key+"_Site").attr('data-name').replace(' Site', ''), content: null, container: 'body', customClass:'hbg'+key })
+  new bootstrap.Popover($("#"+key+"_Site").parent(), { trigger:'hover', placement:'top', title : $("#"+key+"_Site").attr('data-name').replace(' Site', ''), content: vals[1], container: 'body', customClass:'hbg'+key })
 });
 
 var beforePan
@@ -70,7 +93,7 @@ beforePan = function(oldPan, newPan){
           
 var stopHorizontal = false
   , stopVertical = false
-  , gutterWidth = 600 
+  , gutterWidth = 900 
   , gutterHeight = 600 
 // Computed variables
   , sizes = this.getSizes()
