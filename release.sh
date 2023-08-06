e258b4f50fb9f280df0f19a973bbf0c591e95356
realmatic <admin@realmatic.com>
2023-08-05 18:28:44 -0700

    gallery

---
+++ release.sh
@@ -0,0 +1,7 @@
+# set DEBUG false, production ASSET URL 
+
+# wget --no-check-certificate -O index.html $REALMATIC
+
+wget --no-check-certificate -O page/gallery.html $REALMATIC/page/gallery
+
+# wget --no-check-certificate -O page/.html $REALMATIC/page/
