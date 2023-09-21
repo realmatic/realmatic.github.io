bff69746fc416d2acc8c8f9ccfc8fdfa795885a2
realmatic <admin@realmatic.com>
2023-09-20 18:13:11 -0700

    favicon update

--- release.sh
+++ release.sh
@@ -1,8 +1,9 @@
 # set DEBUG false, production ASSET URL 
 
-# wget --no-check-certificate -O index.html $REALMATIC
+wget --no-check-certificate -O index.html $REALMATIC
 
-wget --no-check-certificate -O page/gallery.html $REALMATIC/page/gallery
+#wget --no-check-certificate -O page/gallery.html $REALMATIC/page/gallery
+
 wget --no-check-certificate -O page/23-sfcmap.html $REALMATIC/page/23-sfcmap
 
 # wget --no-check-certificate -O page/.html $REALMATIC/page/
