06f80d00d08436fde1761a23b128f6a31978910a
realmatic <admin@realmatic.com>
2025-06-20 18:13:25 -0700

    start chemistry

---
+++ assets/chemistry/import-periodic-table-lookup.js
@@ -0,0 +1,5 @@
+// todo process csv to json to needed format, key by symbol
+import elementData from './periodic-table-lookup.json' with { type: 'json' };
+const word = elementData.order;
+alert(word)
+
