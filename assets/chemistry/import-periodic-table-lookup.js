f1cd2ef0a747dab1cd343d7cdcbd2edf74447fde
realmatic <admin@realmatic.com>
2025-06-26 18:29:12 -0700

    commit

--- assets/chemistry/import-periodic-table-lookup.js
+++ assets/chemistry/import-periodic-table-lookup.js
@@ -1,5 +1,17 @@
-// todo process csv to json to needed format, key by symbol
-import elementData from './periodic-table-lookup.json' with { type: 'json' };
-const word = elementData.order;
-alert(word)
+const dropdown = document.getElementById('chemElements');
+dropdown.innerHTML = '';
+
+//todo, function to build dropdown select
+
+fetch('/assets/chemistry/periodic-table-lookup.json')
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach(ce => {
+      dropdown.innerHTML += ce.name + ' ';
+    });
+    //console.log(dropdown.innerHTML)
+  })
+  .catch((error) => {
+    console.error('Error Fetching JSON File:', error);
+  });
 
